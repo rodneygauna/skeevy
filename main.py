4b24d2e8ca9ac5bf33a708f3fe9eb4a213530bdd
Rodney Gauna <rodneygauna@gmail.com>
2021-05-25 22:23:04 -0700

    main function start

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+Skeevy - the first python pet medication tracer.
+
+Author: rodneygauna
+GitHub: https://github.com/rodneygauna/skeevy
+"""
+
+
+def main():
+    """Quick main function for the skeey application."""
+    print("FUCK")
+
+
+if __name__ == "__main__":
+    main()
