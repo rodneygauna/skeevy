053412dd30eaf10aa12a6a5f5ab88008afe10a7f
Rodney Gauna <rodneygauna@gmail.com>
2021-05-30 11:55:41 -0700

    inserting a person works

--- sql_tables.py
+++ sql_tables.py
@@ -1,21 +1,25 @@
 import sqlite3
 
-conn = sqlite3.connect('skeevy.db')
-c = conn.cursor()
+create_table_person = """
+CREATE TABLE IF NOT EXISTS person(
+    id INTEGER NOT NULL PRIMARY KEY,
+    firstname TEXT NOT NULL,
+    middlename TEXT NULL,
+    lastname TEXT NOT NULL,
+    suffix TEXT NULL,
+    email TEXT NOT NULL UNIQUE,
+    status TINYINT(1) NOT NULL
+);"""
 
+insert_entry_person = """
+INSERT INTO person (firstname, lastname, email, status) VALUES (
+    'Rodney',
+    'Gauna',
+    'rodneygauna@gmail.com',
+    1
+);"""
 
-def create_table_person():
-    c.execute('CREATE TABLE IF NOT EXISTS person(id INTEGER NOT NULL PRIMARY KEY, firstname TEXT NOT NULL, middlename TEXT NULL, lastname TEXT NOT NULL, suffix TEXT NULL, email TEXT NOT NULL UNIQUE, status TINYINT(1) NOT NULL);')
-
-
-def data_entry_person():
-    c.execute("INSERT INTO person (firstname, lastname, email, status) VALUES ('Rodney', 'Gauna', 'rodneygauna@gmail.com', 1);")
-    conn.commit()
-    c.close()
-    conn.close()
-
-
-create_table_person()
-
-
-data_entry_person()
+with sqlite3.connect("skeevy.db") as connection:
+    cursor = connection.cursor()
+    cursor.execute(create_table_person)
+    cursor.execute(insert_entry_person)
