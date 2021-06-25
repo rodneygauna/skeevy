1e3cf72ab303d456d727214ecd8c407b5cdb1da3
Rodney Gauna <rodneygauna@gmail.com>
2021-06-24 23:25:11 -0700

    user and pet DB models

---
+++ website/models.py
@@ -0,0 +1,20 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+
+class Pet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150))
+    createdate = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    firstname = db.Column(db.String(150))
+    lastname = db.Column(db.String(150))
+    createdate = db.Column(db.DateTime(timezone=True), default=func.now())
+    pets = db.relationship('Pet')
