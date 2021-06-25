b5e0de52a66f3e1e2ff9dcab69bf371ad73dd439
Rodney Gauna <rodneygauna@gmail.com>
2021-06-24 23:07:56 -0700

    formatting fixes

--- website/auth.py
+++ website/auth.py
@@ -24,22 +24,17 @@ def signup():
 
         if len(email) < 4:
             flash(' Email must be greater than 3 characters', category='error')
-
-        elif len(firstname) <2:
+        elif len(firstname) < 2:
             flash(' First name must be greater than 1 characters',
                   category='error')
-
-        elif len(lastname) <2:
+        elif len(lastname) < 2:
             flash(' Last name must be greater than 1 characters',
                   category='error')
-
         elif password1 != password2:
             flash(' Passwords don\'t match.', category='error')
-
-        elif len(password1) <1:
+        elif len(password1) < 1:
             flash(' Password must be greater than 1 character',
                   category='error')
-
         else:
             # add user to db
             pass
