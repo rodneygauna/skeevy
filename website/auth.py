2454b484274d6e8f441d9622135bcb012872b433
Rodney Gauna <rodneygauna@gmail.com>
2021-06-26 10:30:35 -0700

    user can now log in

--- website/auth.py
+++ website/auth.py
@@ -28,7 +28,19 @@ def login():
     # Gets the data from the form and saves as variables
     if request.method == 'POST':
         email = request.form.get('email')
-        password1 = request.form.get('password')
+        password = request.form.get('password')
+
+        # Checks if the user's email is on file
+        user = User.query.filter_by(email=email).first()
+        # Checks if the password is correct
+        if user:
+                if check_password_hash(user.password, password):
+                    flash(' Logged in successfully!', category='success')
+                else:
+                    flash(' Incorrect password, try again.', category='error')
+        else:
+            flash(' Account not found. Check if email is correct and try again.',
+                  category='error')
 
     return render_template("login.html")
 
@@ -50,8 +62,12 @@ def signup():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
+        # Checks if email already exists
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash(' Email is already in use.', category='error')    # security issue - should look at some better wording
         # Validation logic for the submit form
-        if len(email) < 4:
+        elif len(email) < 4:
             flash(' Email must be greater than 3 characters', category='error')
         elif len(firstname) < 2:
             flash(' First name must be greater than 1 characters',
