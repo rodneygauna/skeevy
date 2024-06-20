00bc8d3aba320b62c26670f01638a86465972bdc
Rodney Gauna <rodneygauna@gmail.com>
2024-06-19 22:42:27 -0700

    able to connect to MongoDB now

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,18 @@
 const express = require("express");
-const dotenv = require("dotenv").config();
-const connectDB = require("./config/db");
-
-connectDB();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config({ path: "../.env" });
 
 const app = express();
+
+// Start server and connect to MongoDB
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, async () => {
+  console.log(`Server running on port ${PORT}`);
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+});
