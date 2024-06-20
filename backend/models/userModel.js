d5ccb79c2ae0281edc55469a9f2569737108968b
Rodney Gauna <rodneygauna@gmail.com>
2024-06-19 21:32:05 -0700

    db config and user model

---
+++ backend/models/userModel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    first_name: {
+      type: String,
+      required: [true, "First name is required"],
+    },
+    last_name: {
+      type: String,
+      required: [true, "Last name is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    password_hash: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const connectDB = require("./config/db");
+
+connectDB();
 
 const app = express();
