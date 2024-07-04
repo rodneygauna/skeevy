b74881e7c11103f108e8a580acfc9bc29aec494e
Rodney Gauna <rodneygauna@gmail.com>
2024-07-04 16:20:11 -0700

    started the db

--- app/models/petModel.py
+++ app/models/petModel.py
@@ -3,12 +3,12 @@ from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 from app.database import Base
 
-class User(Base):
+class Pet(Base):
     __tablename__ = "pets"
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
-    birthdate = Column(Date)
+    birthdate = Column(DateTime)
     user_id = Column(Integer, ForeignKey("users.id"))
 
-    user = relationship("User", back_populates("pets"))
+    user = relationship("User", back_populates="pets")
 
