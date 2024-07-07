337d53a51d03d5c0937fe629a58ce73674d5f334
Rodney Gauna <rodneygauna@gmail.com>
2024-07-07 16:35:10 -0700

    working through the authorization logic

--- backend/controllers/authController.py
+++ backend/controllers/authController.py
@@ -1,9 +1,23 @@
 """backend/controllers/authController.py"""
+from datetime import datetime, timedelta, timezone
+from typing import Annotated
+
+import jwt
+from jwt.exceptions import InvalidTokenError
+from fastapi import Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from passlib.context import CryptContext
 
 from models.userModel import User as UserModel
-from schemas.userSchema import User as UserSchema
+from schemas.userSchema import (
+    User as UserSchema,
+    UserCreate as UserCreateSchema,
+)
+
+# TODO: Move these to a .env file and generate a new secret key
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 
 # TODO: Look at using just bcrypt instead of passlib
@@ -25,3 +39,56 @@ def create_user(db: Session, user: UserSchema):
     db.commit()
     db.refresh(db_user)
     return db_user
+
+
+def verify_password(plain_password: str, hashed_password: str):
+    """Verify the password provided by the user."""
+    return bcrypt_context.verify(plain_password, hashed_password)
+
+
+def get_user(db, email: str):
+    if email in db:
+        user_dict = db[email]
+        return UserCreateSchema(**user_dict)
+
+
+def authenticate_user(db: Session, email: str, password: str):
+    """Authenticate a user and return the user if successful."""
+    user = db.query(UserModel).filter(UserModel.email == email).first()
+    if not user:
+        return False
+    if not verify_password(password, user.password):
+        return False
+    return user
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    """Create an access token for the user."""
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email: str = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+        token_data = TokenData(email=email)
+    except InvalidTokenError:
+        raise credentials_exception
+    user = get_user(db, email=token_data.email)
+    if user is None:
+        raise credentials_exception
+    return user
--- backend/routes/authRoute.py
+++ backend/routes/authRoute.py
@@ -1,10 +1,15 @@
 """backend/routes/authRoute.py"""
+from typing import Annotated
+
 from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
 from models.userModel import User as UserModel
 from models.database import get_db
-from controllers.authController import create_user
+from controllers.authController import (
+    create_user, verify_password, authenticate_user,
+)
 from schemas.userSchema import UserCreate, UserBase
 
 
@@ -16,7 +21,7 @@ router = APIRouter(
 
 
 @router.post("/", response_model=UserBase)
-async def create_users(user: UserCreate, db: Session = Depends(get_db)):
+async def create_users(db: Session = Depends(get_db), user: UserCreate):
     """Create a new user in the database."""
     db_user = db.query(UserModel).filter(UserModel.email == user.email).first()
     if db_user:
@@ -24,3 +29,18 @@ async def create_users(user: UserCreate, db: Session = Depends(get_db)):
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="User already registered")
     return create_user(db=db, user=user)
+
+
+@router.post("/token")
+async def login_for_access_token(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(get_db)
+) -> Token:
+    """Authenticate a user and return an access token."""
+    user = authenticate_user(
+        db=db, email=form_data.username, password=form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password")
+    return {"access_token": user.email, "token_type": "bearer"}
