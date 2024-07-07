f5cd27c605f9158bf9d032ddf90127ebcef9255c
Rodney Gauna <rodneygauna@gmail.com>
2024-07-06 22:05:54 -0700

    hashing the password

--- backend/controllers/authController.py
+++ backend/controllers/authController.py
@@ -1,13 +1,26 @@
 """backend/controllers/authController.py"""
 from sqlalchemy.orm import Session
+from passlib.context import CryptContext
 
 from models.userModel import User as UserModel
 from schemas.userSchema import User as UserSchema
 
 
+# TODO: Look at using just bcrypt instead of passlib
+# See issue: https://github.com/pyca/bcrypt/issues/684#issuecomment-1902590553
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
 def create_user(db: Session, user: UserSchema):
     """Create a new user in the database."""
-    db_user = UserModel(**user.model_dump())
+    db_user = UserModel(
+        email=user.email,
+        password=bcrypt_context.hash(user.password),
+        first_name=user.first_name,
+        last_name=user.last_name,
+        phone_number=user.phone_number,
+        is_active=True
+    )
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
